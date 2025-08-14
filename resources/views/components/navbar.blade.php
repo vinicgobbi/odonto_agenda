bd407df576472a4897ee3efb674991498fe2987d
vinicgobbi <vinicius.cgobbi2004@gmail.com>
2025-08-14 14:59:21 -0300

    fix: Remove altura mínima do contêiner principal na navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -94,7 +94,7 @@
 </nav>
 
 
-<div id="main-container" class="d-flex vh-100">
+<div id="main-container" class="d-flex">
 
 <!-- SIDEBAR DESKTOP -->
 <nav class="p-3 d-none d-lg-flex flex-column align-items-center" style="width: 250px; background-color: var(--blue-color);">
